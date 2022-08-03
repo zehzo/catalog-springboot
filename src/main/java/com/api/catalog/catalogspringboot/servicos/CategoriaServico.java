06371e00ab9499fbfc6f5983cbf26af37d8ae2a5
zehzo <edson.ferreiraj@hotmail.com>
2022-08-03 14:58:54 -0300

    adicionando arquivo import.sql para povoar o bd

--- src/main/java/com/api/catalog/catalogspringboot/servicos/CategoriaServico.java
+++ src/main/java/com/api/catalog/catalogspringboot/servicos/CategoriaServico.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.api.catalog.catalogspringboot.entidades.Categoria;
 import com.api.catalog.catalogspringboot.repositorios.CategoriaRepositorio;
@@ -14,6 +15,7 @@ public class CategoriaServico {
 	@Autowired
 	private CategoriaRepositorio repositorio;
 	
+	@Transactional(readOnly = true)
 	public List<Categoria> findAll(){
 		return repositorio.findAll();
 	}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_categoria(name) VALUES ('Livros');
+INSERT INTO tb_categoria(name) VALUES ('Eletronicos');
+INSERT INTO tb_categoria(name) VALUES ('Computadores');
